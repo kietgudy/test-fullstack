812a2e0e711ed01c0542dba252320f33212cfbfa
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-14 12:04:42 +0700

    set up axios & fix CORS

---
+++ react-javascript/src/utils/axios.custom.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// Set config defaults when creating the instance
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL
+  });
+  
+  // Add a request interceptor
+  instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
+  export default instance
+  // Alter defaults after instance has been created
+//   instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
